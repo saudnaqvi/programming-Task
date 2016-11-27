c6ebc5e3c6aba9c660a6f7b3763de9fd8476793b
saudnaqvi <saudnaqvi82@hotmail.com>
2016-11-27 09:57:02 +0500

    Add files via upload

---
+++ test.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+@author: saud
+
+"""
+
+import pandas as pd
+import numpy as np
+
+def match_supplier_name(supplierName, invoiceData):
+    for k in range(0,len(invoiceData)):
+        if supplierName == invoiceData[k].split()[1][1:-1]:
+            return True            
+    return False
+    
+# read suppliernames and invoice data 
+df            = pd.read_csv('suppliernames.txt')
+supplierNames = df.SupplierName
+
+data          = pd.read_csv('invoice.txt', error_bad_lines=False, warn_bad_lines=False)
+mask          = np.column_stack([data[row].str.contains(r"\bword\b", na=False) for row in data])
+
+location      = np.where(mask[0])
+invoiceData   = data.ix[:,int(location[0][0])]
+
+wordMatches   = 0
+i             = 0
+# match supplier names to invoice data
+while i < len(supplierNames): 
+    for j in range(0,len(supplierNames[i].split())):
+        if (match_supplier_name(supplierNames[i].split()[j], invoiceData) == True):
+            wordMatches = wordMatches+1            
+        else:
+            break
+    if wordMatches == len(supplierNames[i].split()):
+        print supplierNames[i]
+        break
+    else:
+        wordMatches = 0
+        i=i+1
+        #continue
+
+        
+                    
